1f8671d7f809cf64f512de107eabb3d0e92e6d94
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-27 20:50:07 +0900

    Feat :: ProfileView UI구현 중간 커밋

--- Took_Talk_V2/Sources/View/Profile/ProfileView.swift
+++ Took_Talk_V2/Sources/View/Profile/ProfileView.swift
@@ -9,7 +9,47 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .resizable()
+                            .background(.blue)
+                            .frame(width: 100, height: 100)
+                            .clipShape(RoundedRectangle(cornerRadius: 50, style: .continuous))
+                        
+                        VStack(alignment: .leading) {
+                            Text("양송이 모자")
+                                .font(.pretendard(20, weight: .light))
+                            
+                            Text("010-9677-6210")
+                                .font(.pretendard(15, weight: .light))
+
+                            Button {
+                                
+                            } label: {
+                                Text("수정하기")
+                                    .foregroundStyle(.white)
+                                    .font(.pretendard(15, weight: .bold))
+                            }
+                            .frame(width: 80, height: 28)
+                            .background(Color("myOrange"))
+                            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+
+                        }
+                        
+                        Spacer()
+                        
+                    }
+                    .frame(height: 130, alignment: .center)
+                    .padding(.horizontal, 40)
+                }
+                
+            }
+            .navigationTitle("프로필")
+            .navigationBarTitleDisplayMode(.large)
+        }
     }
 }
 
