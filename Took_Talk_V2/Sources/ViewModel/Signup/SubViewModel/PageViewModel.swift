1b28412dec56d966031bce3980c884f7c43da6d3
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-27 20:50:34 +0900

    Create PageViewModel.swift

---
+++ Took_Talk_V2/Sources/ViewModel/Signup/SubViewModel/PageViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  PageViewModel.swift
+//  Took_Talk
+//
+//  Created by 최시훈 on 3/8/24.
+//
+
+import Foundation
+
+class PageViewModel: ObservableObject {
+    @Published var progress: Int = 1 {
+        didSet {
+            if progress > 6 {
+            progress = 6
+        }
+            
+        }
+    }
+
+    func next() {
+        progress += 1
+    }
+    
+    func back() {
+        progress -= 1
+    }
+}
