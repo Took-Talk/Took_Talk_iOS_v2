5a03dbd16afb65d4ac3067e26e209485afbfb9a3
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-31 23:44:03 +0900

    Create Text.swift

---
+++ Took_Talk_V2/Sources/Extension/Text.swift
@@ -0,0 +1,16 @@
+//
+//  Text.swift
+//  Took_Talk_V2
+//
+//  Created by 최시훈 on 3/30/24.
+//
+
+import SwiftUI
+
+extension Text {
+    func placeholderStyle() -> Text {
+        self
+            .foregroundColor(Color(.systemGray4))
+            .font(.pretendard())
+    }
+}
