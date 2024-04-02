db72b22f03ede8773c3d2b3c8a45a38b6145f26f
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-02 20:35:44 +0900

    feat :: RoundedCornerView 생성

---
+++ Took_Talk_V2/Sources/View/Style/RoundedCorner.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedCorner.swift
+//  Took_Talk_V2
+//
+//  Created by 최시훈 on 4/2/24.
+//
+
+import SwiftUI
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
