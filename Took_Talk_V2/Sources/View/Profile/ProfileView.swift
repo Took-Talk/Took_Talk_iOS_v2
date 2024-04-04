c04d71d40cf2a26748c96365aab092cfc0d97bf2
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-04 22:11:36 +0900

    feat :: ProfileView 구현 완료

--- Took_Talk_V2/Sources/View/Profile/ProfileView.swift
+++ Took_Talk_V2/Sources/View/Profile/ProfileView.swift
@@ -47,7 +47,7 @@ struct ProfileView: View {
                     
                     VStack(spacing: 25) {
                         ProfileInformationView(
-                            imageName: "calendar", 
+                            imageName: "calendar",
                             text: "나이",
                             info: "\(viewModel.age)"
                         )
@@ -58,48 +58,53 @@ struct ProfileView: View {
                             info: "\(viewModel.gender)"
                         )
                         HStack {
-                            Image("Interest")
-                                .frame(width: 30, height: 30)
-                                .foregroundStyle(Color.myOrange)
-                            
-                            Text("관심사")
-                                .foregroundStyle(Color.myOrange)
-                                .padding(.leading, 15)
-                                .font(.pretendard(18))
-                            
-                            WrappingHStack(viewModel.interests) { arr in
-                                Text("\(arr)")
-                                    .font(.pretendard(15))
-                                    .foregroundStyle(.black)
-                                    .padding(.horizontal, 4)
-                                    .frame(height: 30)
-                                    .foregroundColor(Color(.systemGray4))
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 20)
-                                            .stroke(Color(.systemGray3), lineWidth: 1)
-                                    )
-                                    .padding(.vertical, 3)
+                            VStack {
+                                HStack {
+                                    Image("Interest")
+                                        .frame(width: 30, height: 30)
+                                        .foregroundStyle(Color.myOrange)
+                                    
+                                    Text("관심사")
+                                        .foregroundStyle(Color.myOrange)
+                                        .padding(.leading, 15)
+                                        .font(.pretendard(18))
+                                }
+                                Spacer()
                             }
                             
-                            Button {
-                                viewModel.isPressed.toggle()
-                            } label: {
-                                Image(systemName: "arrowtriangle.right.fill")
-                                    .resizable()
-                                    .frame(width: 20, height: 20)
-                                    .foregroundStyle(.black)
+                            if viewModel.isPressed {
+                                MultiLineHStack(items: viewModel.interests, maxWidth: 200)
+                                    .frame(maxWidth: .infinity, alignment: .top)
+
+                            } else {
+                                SingleLineHStack(items: viewModel.interests, maxWidth: 200)
+                                    .frame(maxWidth: .infinity, alignment: .top)
                             }
-                            .rotationEffect(Angle(degrees: viewModel.isPressed ? 90 : 0))
-                            .animation(.easeInOut(duration: 0.2))
-                            .padding(.trailing, 20)
-                        }
-                        if viewModel.isPressed {
                             
+                            Spacer()
+
+                            VStack {
+                                Button {
+                                    viewModel.isPressed.toggle()
+                                } label: {
+                                    Image(systemName: "arrowtriangle.right.fill")
+                                        .resizable()
+                                        .frame(width: 15, height: 15)
+                                        .foregroundStyle(.black)
+                                }
+                                .rotationEffect(Angle(degrees: viewModel.isPressed ? 90 : 0))
+                                .animation(.easeInOut(duration: 0.1), value: viewModel.isPressed)
+                                .padding(.trailing, 15)
+//                                .frame()
+                                Spacer()
+                            }
                         }
+                        .animation(.easeInOut(duration: 0.1), value: viewModel.isPressed)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                         
                         ProfileInformationView(
                             imageName: "doc.text.magnifyingglass",
-                            text: "MBTI", 
+                            text: "MBTI",
                             info: "\(viewModel.mbti)"
                         )
                         
@@ -148,16 +153,14 @@ struct ProfileView: View {
                     
                 }
             }
-            .navigationTitle("프로필")
-            .navigationBarTitleDisplayMode(.large)
-            .onAppear {
-                viewModel.getUserInfo()
-            }
-            .refreshable {
-                viewModel.getUserInfo()
-            }
         }
-        
+        .navigationTitle("프로필")
+        .onAppear {
+            viewModel.getUserInfo()
+        }
+        .refreshable {
+            viewModel.getUserInfo()
+        }
     }
 }
 
