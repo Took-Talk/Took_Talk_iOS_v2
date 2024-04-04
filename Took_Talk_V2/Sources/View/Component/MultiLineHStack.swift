25f29532bae303d5bda36a24074a085725aa07a7
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-04 21:30:41 +0900

    feat :: MultiLineHStack구현

---
+++ Took_Talk_V2/Sources/View/Component/MultiLineHStack.swift
@@ -0,0 +1,61 @@
+//
+//  MultiLineHStack.swift
+//  Took_Talk_V2
+//
+//  Created by 최시훈 on 4/4/24.
+//
+
+import SwiftUI
+
+struct MultiLineHStack: View {
+    let items: [String]
+    let maxWidth: CGFloat
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            ForEach(wrapItems(), id: \.self) { rowItems in
+                HStack(spacing: 3) {
+                    ForEach(rowItems, id: \.self) { item in
+                        Text(item)
+                            .font(.system(size: 15))
+                            .foregroundColor(Color.black)
+                            .padding(.horizontal, 4)
+                            .frame(height: 30)
+                            .foregroundColor(Color(.systemGray4))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color(.systemGray3), lineWidth: 1)
+                            )
+                            .padding(.vertical, 3)
+                    }
+                    Spacer()
+                }
+            }
+        }
+    }
+    
+    func wrapItems() -> [[String]] {
+        var result: [[String]] = []
+        var currentRow: [String] = []
+        var currentWidth: CGFloat = 0
+        
+        for item in items {
+            let itemWidth = item.widthOfString(usingFont: .systemFont(ofSize: 17)) + 10
+            
+            if currentWidth + itemWidth > maxWidth {
+                result.append(currentRow)
+                currentRow = [item]
+                currentWidth = itemWidth
+            } else {
+                currentRow.append(item)
+                currentWidth += itemWidth
+            }
+        }
+        
+        if !currentRow.isEmpty {
+            result.append(currentRow)
+        }
+        
+        return result
+    }
+}
