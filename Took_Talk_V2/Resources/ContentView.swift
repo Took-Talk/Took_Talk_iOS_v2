2e2ad4ed7b80c0af819d669cbbe4f391caf64fa3
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-05-07 21:49:37 +0900

    remove: publishers

--- Took_Talk_V2/Resources/ContentView.swift
+++ Took_Talk_V2/Resources/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     
     var body: some View {
                 Text("Press to Expand:")
+            .font(.custom("Pretendard-Bold", size: 20))
                 
     }
 }
--- Took_Talk_V2/Sources/View/Signup/SubView/PageView.swift
+++ Took_Talk_V2/Sources/View/Signup/SubView/PageView.swift
@@ -10,28 +10,6 @@ import SwiftUI
 struct PageView: View {
     @StateObject var viewModel: PageViewModel
     
-    let phoneNumberVerifiedPublisher = NotificationCenter.default.publisher(for: .phoneNumberVerified)
-    let phoneNumberDidChangePublisher = NotificationCenter.default.publisher(for: .phoneNumberDidChange)
-    
-    let passwordCheckPublisher =  NotificationCenter.default.publisher(for: .passwordCheck)
-    let passwordPublisher = NotificationCenter.default.publisher(for: .passwordDidChange)
-    
-    let nicknameCheckPublisher =  NotificationCenter.default.publisher(for: .nicknameCheck)
-    let nicknamePublisher = NotificationCenter.default.publisher(for: .nicknameDidChange)
-    let genderPublisher = NotificationCenter.default.publisher(for: .genderDidChange)
-    let agePublisher = NotificationCenter.default.publisher(for: .ageDidChange)
-    
-    //    let mbtiCheckPublisher =  NotificationCenter.default.publisher(for: .mbtiCheck)
-    let mbtiPublisher = NotificationCenter.default.publisher(for: .mbtiDidChange)
-    
-    //    let interestCheckPublisher =  NotificationCenter.default.publisher(for: .interestCheck)
-    let interestPublisher = NotificationCenter.default.publisher(for: .interestsDidChange)
-    
-    //    let bioCheckPublisher =  NotificationCenter.default.publisher(for: .bioCheck)
-    let bioPublisher = NotificationCenter.default.publisher(for: .bioDidChange)
-    
-    let imagePublisher = NotificationCenter.default.publisher(for: .imageDidChange)
-    
     var body: some View {
         VStack {
             ProgressView(value: Float(viewModel.progress), total: 7) {
@@ -74,42 +52,5 @@ struct PageView: View {
         }
         .padding(.top, 20)
         .padding(.horizontal, 20)
-        .onReceive(phoneNumberVerifiedPublisher) { _ in
-            viewModel.isCertificate = true
-        }
-        .onReceive(phoneNumberDidChangePublisher) { notification in
-            viewModel.number = notification.object as? String ?? ""
-        }
-        
-        .onReceive(passwordCheckPublisher) { _ in
-            viewModel.isSamePassword = true
-            print(viewModel.isSamePassword)
-            
-        }
-        .onReceive(passwordPublisher) { notification in
-            viewModel.password = notification.object as? String ?? ""
-        }
-        
-        .onReceive(nicknamePublisher) { notification in
-            viewModel.nickname = notification.object as? String ?? ""
-        }
-        .onReceive(genderPublisher) { notification in
-            viewModel.gender = notification.object as? String ?? ""
-        }
-        .onReceive(agePublisher) { notification in
-            viewModel.age = notification.object as? String ?? ""
-        }
-        .onReceive(mbtiPublisher) { notification in
-            viewModel.mbti = notification.object as? String ?? ""
-        }
-        .onReceive(interestPublisher) { notification in
-            viewModel.interests = notification.object as? [String] ?? []
-        }
-        .onReceive(bioPublisher) { notification in
-            viewModel.bio = notification.object as? String ?? ""
-        }
-        .onReceive(imagePublisher) { notification in
-            viewModel.image = notification.object as? UIImage ?? UIImage()
-        }
     }
 }
