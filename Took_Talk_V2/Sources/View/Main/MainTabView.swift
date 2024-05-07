c3ceec655c900e118f39122e81dd30604b5a6c94
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-05-07 20:34:37 +0900

    feat: MainTabVIew

--- Took_Talk_V2/Sources/View/Main/MainTabView.swift
+++ Took_Talk_V2/Sources/View/Main/MainTabView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MainTabView: View {
     var body: some View {
         NavigationStack {
-            TabView() {
+            TabView {
                 FriendView()
                     .tabItem {
                         Image(systemName: "person.3.fill")
